0a52380b91dac3d648e3f5522467ef7af2a799e5
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-08 23:10:11 -0300

    test: testando criacao para o primeiro usecase

---
+++ src/usecase/customer/find/find.customer.spec.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize-typescript"
+
+describe("Test find customer use case", () => {
+
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory",
+      logging: false,
+      sync: { force: true },
+    });
+  });
+})
