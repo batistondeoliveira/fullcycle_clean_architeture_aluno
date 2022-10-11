28ef2aa869c497b6e580467b2948b046c5ef0f90
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-11 08:44:17 -0300

    test: adicionando notification em customer

---
+++ src/domain/@shared/notification/notification.spec.ts
@@ -0,0 +1,41 @@
+import Notification from "./notification";
+
+describe("Unit tests for notifications", () => {
+
+  it("should create errors", () => {
+    const notification = new Notification();
+    const error = {
+      message: "Error message",
+      context: "customer",
+    }
+    notification.addError(error);
+
+    expect(notification.messages("customer")).toBe("customer: Error message,");
+
+    const error2 = {
+      message: "Error message 2",
+      context: "customer",
+    }
+
+    notification.addError(error2);
+
+    expect(notification.messages("customer")).toBe(
+      "customer: Error message,customer: Error message 2,"
+    );
+
+    const error3 = {
+      message: "Error message 3",
+      context: "order",
+    }
+
+    notification.addError(error3);
+
+    expect(notification.messages("customer")).toBe(
+      "customer: Error message,customer: Error message 2,"
+    );
+
+    expect(notification.messages()).toBe(
+      "customer: Error message,customer: Error message 2,order: Error message 3,"
+    );
+  });
+});
