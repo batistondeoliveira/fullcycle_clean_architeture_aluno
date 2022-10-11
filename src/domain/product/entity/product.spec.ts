1e9763203a46909a89116b85093fe76cb30fb9cd
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-10-11 09:35:23 -0300

    test: desafio notification pattern em Products

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -4,20 +4,35 @@ describe("Product unit tests", () => {
   
   it("should throw error when id is empty", () => {    
     expect(() => {      
-      let product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");    
-  })
+      new Product("", "Product 1", 100);
+    }).toThrowError("product: Id is required");    
+  });
 
   it("should throw error when name is empty", () => {    
     expect(() => {      
-      let product = new Product("123", "", 100);
-    }).toThrowError("Name is required");    
-  })
+      new Product("123", "", 100);
+    }).toThrowError("product: Name is required");    
+  });
+
+  it("should throw error when id and name is empty", () => {    
+    expect(() => {      
+      new Product("", "", 100);
+    }).toThrowError("product: Id is required,product: Name is required");    
+  });
 
   it("should throw error when price is less than zero", () => {    
     expect(() => {      
-      let product = new Product("123", "Product 1", -1);
-    }).toThrowError("Price must be greater than zero");    
+      new Product("123", "Product 1", -1);
+    }).toThrowError("product: Price must be greater than zero");    
+  })
+
+  it("should throw error when id, name and price is less than zero", () => {    
+    expect(() => {      
+      new Product("", "", -1);
+    }).toThrowError(
+      "product: Id is required,product: Name is required,"+
+      "product: Price must be greater than zero"
+    );    
   })
 
   it("should change name", () => {    
